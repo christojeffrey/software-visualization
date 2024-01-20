c08ff22e8f91cbd59e10f45c5d32e8885b40e897
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2024-01-20 23:19:01 +0700

    checkpoint: removing linter error. not all

--- svelte/src/routes/test/script.ts
+++ svelte/src/routes/test/script.ts
@@ -100,9 +100,29 @@ const SVGMARGIN = 50;
 const PADDING = 10;
 // const MINIMUMNODESIZE = 10;
 
-function ticked() {}
+function onDragStart(d, allSimulation: any[]) {
+	// if (!d3.event.active) {
+	allSimulation.forEach((simul) => {
+		simul.alphaTarget(0.5).restart();
+	});
+	// }
+}
 
-function createInnerSimulation(nodes, canvas, allSimulation, parentNode) {
+function onDrag(d, allSimulation) {
+	d.fx = d3.event.x;
+	d.fy = d3.event.y;
+}
+
+function onDragEnd(d: any, allSimulation: any[]) {
+	// if (!d3.event.active) {
+	allSimulation.forEach((simul) => {
+		simul.alphaTarget(0).restart();
+	});
+	// }
+	d.fx = null;
+	d.fy = null;
+}
+function createInnerSimulation(nodes: any, canvas: any, allSimulation: any, parentNode: any) {
 	console.log(nodes);
 	// use this instead of forEach so that it is passed by reference.
 
@@ -125,7 +145,7 @@ function createInnerSimulation(nodes, canvas, allSimulation, parentNode) {
 		.enter()
 		.append('g')
 		.attr('class', 'node')
-		.attr('id', (d) => d.id);
+		.attr('id', (d: any) => d.id);
 
 	membersContainerElement.call(
 		d3
@@ -140,7 +160,7 @@ function createInnerSimulation(nodes, canvas, allSimulation, parentNode) {
 				onDragEnd(d, allSimulation);
 			})
 	);
-	let memberElements = membersContainerElement
+	const memberElements = membersContainerElement
 		.append('rect')
 		.attr('x', 0)
 		.attr('y', 0)
@@ -162,7 +182,106 @@ function createInnerSimulation(nodes, canvas, allSimulation, parentNode) {
 		}
 	}
 }
-export function draw(svgElement, graphData, drawSettings) {
+function linkTicked(linkElements: any) {
+	linkElements
+		.attr('x1', (d: any) => {
+			// change to global coordinates.
+			let result = d.source.x;
+
+			let temp = d.source;
+			while (temp.parent) {
+				result += temp.parent.x;
+				temp = temp.parent;
+			}
+
+			return result;
+		})
+		.attr('y1', (d: any) => {
+			// change to global coordinates.
+			let result = d.source.y;
+
+			let temp = d.source;
+			while (temp.parent) {
+				result += temp.parent.y;
+				temp = temp.parent;
+			}
+
+			return result;
+		})
+		.attr('x2', (d: any) => {
+			// change to global coordinates.
+			let result = d.target.x;
+
+			let temp = d.target;
+			while (temp.parent) {
+				result += temp.parent.x;
+				temp = temp.parent;
+			}
+
+			return result;
+		})
+		.attr('y2', (d: any) => {
+			// change to global coordinates.
+			let result = d.target.y;
+
+			let temp = d.target;
+			while (temp.parent) {
+				result += temp.parent.y;
+				temp = temp.parent;
+			}
+
+			return result;
+		});
+}
+function masterSimulationTicked(graphData: any, containerElement: any, nodeElements: any) {
+	// calculate nodes width and height, x and y. only do this calculation once, on master simulation
+	for (let i = 0; i < graphData.flattenNodes.length; i++) {
+		if (graphData.flattenNodes[i].members) {
+			const members = graphData.flattenNodes[i].members;
+			// members location is relative to the parent.
+			let minX = members[0].x + members[0].cx;
+			let maxX = members[0].x + members[0].cx + members[0].width;
+			let minY = members[0].y + members[0].cy;
+			let maxY = members[0].y + members[0].cy + members[0].height;
+
+			for (let j = 0; j < members.length; j++) {
+				if (members[j].x + members[j].cx < minX) {
+					minX = members[j].x;
+				}
+				if (members[j].x + members[j].cx + members[j].width > maxX) {
+					maxX = members[j].x + members[j].cx + members[j].width;
+				}
+				if (members[j].y + members[j].cy < minY) {
+					minY = members[j].y + members[j].cy;
+				}
+				if (members[j].y + members[j].cy + members[j].height > maxY) {
+					maxY = members[j].y + members[j].cy + members[j].height;
+				}
+			}
+
+			graphData.flattenNodes[i].width = maxX - minX + PADDING * 2;
+			graphData.flattenNodes[i].height = maxY - minY + PADDING * 2;
+			// stands for calculated x and y.
+			graphData.flattenNodes[i].cx = minX - PADDING;
+			graphData.flattenNodes[i].cy = minY - PADDING;
+		} else {
+			graphData.flattenNodes[i].width = 10;
+			graphData.flattenNodes[i].height = 10;
+			//   stands for calculated x and y.
+			graphData.flattenNodes[i].cx = 0;
+			graphData.flattenNodes[i].cy = 0;
+		}
+	}
+
+	containerElement.attr('transform', (d: any) => {
+		return `translate(${d.x},${d.y})`;
+	});
+	nodeElements.attr('width', (d: any) => d.width).attr('height', (d: any) => d.height);
+	// cannot use below as this wouldn't move the children of that group, which is the subgraph
+
+	nodeElements.attr('x', (d: any) => d.cx).attr('y', (d: any) => d.cy);
+}
+export function draw(svgElement: any, graphData: any, drawSettings: any) {
 	console.log('draw');
 	const simulations: any = [];
 
@@ -174,53 +293,8 @@ export function draw(svgElement, graphData, drawSettings) {
 	const simulation = d3.forceSimulation(graphData.nodes);
 	simulation.force('charge', d3.forceManyBody().strength(-300));
 	simulation.force('center', d3.forceCenter(SVGSIZE / 2, SVGSIZE / 2));
-	simulation.on('tick', function ticked() {
-		// calculate nodes width and height, x and y. only do this calculation once, on master simulation
-		for (let i = 0; i < graphData.flattenNodes.length; i++) {
-			if (graphData.flattenNodes[i].members) {
-				const members = graphData.flattenNodes[i].members;
-				// members location is relative to the parent.
-				let minX = members[0].x + members[0].cx;
-				let maxX = members[0].x + members[0].cx + members[0].width;
-				let minY = members[0].y + members[0].cy;
-				let maxY = members[0].y + members[0].cy + members[0].height;
-
-				for (let j = 0; j < members.length; j++) {
-					if (members[j].x + members[j].cx < minX) {
-						minX = members[j].x;
-					}
-					if (members[j].x + members[j].cx + members[j].width > maxX) {
-						maxX = members[j].x + members[j].cx + members[j].width;
-					}
-					if (members[j].y + members[j].cy < minY) {
-						minY = members[j].y + members[j].cy;
-					}
-					if (members[j].y + members[j].cy + members[j].height > maxY) {
-						maxY = members[j].y + members[j].cy + members[j].height;
-					}
-				}
-
-				graphData.flattenNodes[i].width = maxX - minX + PADDING * 2;
-				graphData.flattenNodes[i].height = maxY - minY + PADDING * 2;
-				// stands for calculated x and y.
-				graphData.flattenNodes[i].cx = minX - PADDING;
-				graphData.flattenNodes[i].cy = minY - PADDING;
-			} else {
-				graphData.flattenNodes[i].width = 10;
-				graphData.flattenNodes[i].height = 10;
-				//   stands for calculated x and y.
-				graphData.flattenNodes[i].cx = 0;
-				graphData.flattenNodes[i].cy = 0;
-			}
-		}
-
-		containerElement.attr('transform', (d) => {
-			return `translate(${d.x},${d.y})`;
-		});
-		nodeElements.attr('width', (d) => d.width).attr('height', (d) => d.height);
-		// cannot use below as this wouldn't move the children of that group, which is the subgraph
-
-		nodeElements.attr('x', (d) => d.cx).attr('y', (d) => d.cy);
+	simulation.on('tick', () => {
+		masterSimulationTicked(graphData, containerElement, nodeElements);
 	});
 
 	const canvas = svg.append('g');
@@ -232,7 +306,7 @@ export function draw(svgElement, graphData, drawSettings) {
 		.enter()
 		.append('g')
 		.attr('class', 'nodes')
-		.attr('id', (d) => d.id);
+		.attr('id', (d: any) => d.id);
 
 	const nodeElements = containerElement
 		.append('rect')
@@ -244,82 +318,35 @@ export function draw(svgElement, graphData, drawSettings) {
 		.attr('fill-opacity', '0.1');
 
 	// link
-	let linkElements = canvas
+	const linkElements = canvas
 		.selectAll('line.link')
 		.data(graphData.links)
 		.enter()
 		.append('line')
 		.style('stroke', 'blue')
 		.attr('class', 'link');
-	let linkSimulation = d3
+	const linkSimulation = d3
 		.forceSimulation(graphData.flattenNodes)
 		.force(
 			'link',
 			d3
 				.forceLink(graphData.links)
-				.id((d) => {
+				.id((d: any) => {
 					return d.id;
 				})
 				.strength(0)
 		)
-		.on('tick', (e) => {
-			linkElements
-				.attr('x1', (d) => {
-					// change to global coordinates.
-					let result = d.source.x;
-
-					let temp = d.source;
-					while (temp.parent) {
-						result += temp.parent.x;
-						temp = temp.parent;
-					}
-
-					return result;
-				})
-				.attr('y1', (d) => {
-					// change to global coordinates.
-					let result = d.source.y;
-
-					let temp = d.source;
-					while (temp.parent) {
-						result += temp.parent.y;
-						temp = temp.parent;
-					}
-
-					return result;
-				})
-				.attr('x2', (d) => {
-					// change to global coordinates.
-					let result = d.target.x;
-
-					let temp = d.target;
-					while (temp.parent) {
-						result += temp.parent.x;
-						temp = temp.parent;
-					}
-
-					return result;
-				})
-				.attr('y2', (d) => {
-					// change to global coordinates.
-					let result = d.target.y;
-
-					let temp = d.target;
-					while (temp.parent) {
-						result += temp.parent.y;
-						temp = temp.parent;
-					}
-
-					return result;
-				});
+		.on('tick', () => {
+			linkTicked(linkElements);
 		});
-
+	simulations.push(linkSimulation);
 	// create inner simulation.
 	for (let i = 0; i < graphData.nodes.length; i++) {
 		if (graphData.nodes[i].members) {
 			createInnerSimulation(graphData.nodes[i].members, canvas, simulations, graphData.nodes[i]);
 		}
 	}
+
 	return {
 		simulations,
 		svgElement
