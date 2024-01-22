3b0a1e21146aa99527ae11463898f680e0206618
Adityapnn811 <apn725@gmail.com>
2024-01-22 12:08:00 +0700

    WIP: added comment

--- svelte/src/routes/test/scripts/draw/index.ts
+++ svelte/src/routes/test/scripts/draw/index.ts
@@ -129,6 +129,7 @@ export function draw(svgElement: any, graphData: any, _drawSettings: any) {
 			return `url(#${d.type}Gradient)`;
 		})
 		.attr('class', 'link');
+	// To rotate the gradient, since its linear, I thing I can use only the negative or positive attribute of their coordinates. No need to calculate degrees
 	const linkSimulation = d3
 		.forceSimulation(graphData.flattenNodes)
 		.force(
