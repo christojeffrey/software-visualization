36a0908074442538bbcd749648e8535917372fb8
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2024-04-24 02:53:24 +0700

    feat: able to input from file

--- svelte/src/routes/pack/+page.svelte
+++ svelte/src/routes/pack/+page.svelte
@@ -1,67 +1,109 @@
 <script lang="ts">
 	import {onMount} from 'svelte';
 	import * as d3 from 'd3';
+	import {Input} from '$lib/components/ui/input/index.js';
+	import {Label} from '$lib/components/ui/label/index.js';
 
 	import * as Resizable from '$lib/components/ui/resizable';
 	let svgElement: SVGElement | undefined = undefined;
 
 	type HierarchyData = {
-		name: string;
-		value: number;
+		id: string;
+		count: number;
 		children?: HierarchyData[];
 	};
 	let isMounted = false;
+	function inputDataToHierarchyData(csvText: string): any {
+		// package, class, layer, count
+		const table = d3.csvParse(csvText);
+		const packages: any = new Map<String, any[]>();
+		// get all unique package, and append to table
+		table.forEach(row => {
+			console.log('row', row);
+			if (!(row.package in packages)) {
+				packages[row.package] = [];
+			}
+			packages[row.package].push(row);
+		});
+		const roots: any = [];
+		Object.entries(packages).forEach(([key, value]: any) => {
+			let temporaryTable = [...value];
+			temporaryTable.push({
+				class: key,
+				package: '',
+				count: 1,
+				layer: '',
+			});
+			console.log('temporaryTable');
+			console.log(temporaryTable);
+			const root = d3
+				.stratify()
+				.id((d: any) => d.class)
+				.parentId((d: any) => d.package)(temporaryTable);
+			roots.push(root);
+		});
+		console.log('roots', roots);
 
+		root = roots[5];
+		d3.select(svgElement!).selectChildren().remove();
+	}
 	const data: HierarchyData = {
-		name: 'Eve',
-		value: 10,
+		id: 'Eve',
+		count: 1,
 		children: [
-			{name: 'Cain', value: 10},
+			{id: 'Cain', count: 1},
 			{
-				name: 'Seth',
-				value: 10,
+				id: 'Seth',
+				count: 1,
 				children: [
-					{name: 'Enos', value: 10},
-					{name: 'Noam', value: 10},
+					{id: 'Enos', count: 1},
+					{id: 'Noam', count: 1},
 				],
 			},
-			{name: 'Abel', value: 10},
-			{name: 'Awan', value: 10, children: [{name: 'Enoch', value: 10}]},
-			{name: 'Azura', value: 10},
+			{id: 'Abel', count: 1},
+			{id: 'Awan', count: 1, children: [{id: 'Enoch', count: 1}]},
+			{id: 'Azura', count: 1},
 		],
 	};
+	let root = d3.hierarchy(data) as any;
 	$: {
 		if (isMounted) {
-			const root = d3.hierarchy(data) as any;
-
 			// Construct the treemap layout.
 			const pack = d3.pack<any>();
 			pack.size([500, 500]);
 			pack.padding(2);
 
 			// // Sum and sort the data.
-			root.sum((d: any) => d.value);
+			root.sum((d: any) => d.count);
 			// root.sort((a, b) => b.height - a.height || b.value! - a.value!);
 
-			// // Compute the treemap layout.
+			// Compute the pack layout.
 			pack(root);
-			console.log('root');
-			console.log(root);
+			let canvas = d3.select(svgElement!).append('g').attr('id', 'canvas');
 
-			const canvas = d3.select(svgElement!).append('g').attr('id', 'canvas');
-			// const canvasElement = document.getElementById('canvas')!;
 			root.each((d: any) => {
 				console.log(d);
 				const node = canvas
 					.append('g')
 					.attr('transform', `translate(${d.x},${d.y})`)
-					.attr('class', 'node')
-					.attr('fill', 'red');
+					.attr('class', 'node');
 
 				node
 					.append('circle')
 					.attr('r', d.r)
-					.attr('fill', 'rgba(0, 0, 0, 0.2)')
+					.attr('fill', () => {
+						console.log('d', d);
+						// based on data.layer
+						if (d?.data?.layer === 'Data Source Layer') {
+							return 'rgba(0, 100, 0, 0.2)';
+						} else if (d?.data?.layer === 'Domain Layer') {
+							return 'rgba(100, 0, 0, 0.2)';
+						} else if (d?.data?.layer === 'Presentation Layer') {
+							return 'rgba(0, 0, 100, 0.2)';
+						} else {
+							return 'rgba(0, 0, 0, 0.2)';
+						}
+					})
 					.attr('stroke', 'black');
 
 				node
@@ -70,21 +112,37 @@
 					.attr('fill', 'black')
 					.attr('text-anchor', 'middle')
 					.attr('font-size', '10px')
-					.text(d.data.name);
+					.text(d.data.id);
 			});
 		}
 	}
 	onMount(() => {
 		isMounted = true;
 	});
+
+	function handleFileChange(e: any) {
+		const file = e.target?.files?.[0];
+		if (!file) return;
+		const reader = new FileReader();
+		reader.onload = e => {
+			const text = e.target?.result as string;
+			inputDataToHierarchyData(text);
+		};
+		reader.readAsText(file);
+	}
 </script>
 
-<Resizable.PaneGroup direction="horizontal">
-	<Resizable.Pane>
-		left
-		<svg bind:this={svgElement} class="w-1/2 h-1/2 border-2 border-black" />
-		done
-	</Resizable.Pane>
-	<Resizable.Handle withHandle />
-	<Resizable.Pane>right</Resizable.Pane>
-</Resizable.PaneGroup>
+<div class="p-4 h-full w-full">
+	<Resizable.PaneGroup direction="horizontal">
+		<Resizable.Pane class="p-2" defaultSize={85}>
+			<svg bind:this={svgElement} class="w-full h-full" width="100%" />
+		</Resizable.Pane>
+		<Resizable.Handle />
+		<Resizable.Pane class="p-2"
+			><div class="grid w-full max-w-sm items-center gap-1.5">
+				<Label for="picture" class="font-bold">Input</Label>
+				<Input id="picture" type="file" on:change={handleFileChange} />
+			</div></Resizable.Pane
+		>
+	</Resizable.PaneGroup>
+</div>
