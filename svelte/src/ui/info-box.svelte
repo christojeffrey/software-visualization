aee806a8f18a7fe4a0f4645bf3fca21bf9706acd
Aditya Prawira Nugroho <apn725@gmail.com>
2024-04-26 11:01:07 +0700

    style: Styled the information box

--- svelte/src/ui/info-box.svelte
+++ svelte/src/ui/info-box.svelte
@@ -2,7 +2,7 @@
 </script>
 
 <div id="info-box" class="p-4 rounded-lg shadow-md min-w-72">
-    <h1 id="info-box-name">Name</h1>
-    <h2 id="info-box-heading2">Kind - Role Stereotype</h2>
+    <h1 id="info-box-name" class="font-bold text-xl">Name</h1>
+    <h2 id="info-box-heading2" class="font-semibold">Kind - Role Stereotype</h2>
     <p id="info-box-desc"></p>
 </div>
