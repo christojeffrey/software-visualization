2da8abdb1bb49d0d6b34498d58b82f86571228a4
Aditya Prawira Nugroho <apn725@gmail.com>
2024-05-06 00:40:08 +0700

    chore: cleanup unused function

--- svelte/src/routes/components/scripts/helper/index.ts
+++ svelte/src/routes/components/scripts/helper/index.ts
@@ -22,21 +22,6 @@ export function extractAvailableEdgeType(links: GraphDataEdge[]) {
 	return availableEdgeType;
 }
 
-// Currently, this function combine the weights with undeleted duplicated links in mind
-export function combineWeights(duplicateLinks: Map<string, GraphDataEdge[]>) {
-	for (const [_, edges] of duplicateLinks) {
-		let totalWeight = 0;
-		edges.forEach(edge => {
-			if (!edge.originalWeight) edge.originalWeight = edge.weight;
-			totalWeight += edge.originalWeight;
-		});
-		// Reassign the weight
-		edges.forEach(edge => {
-			edge.weight = totalWeight;
-		});
-	}
-}
-
 export function toHTMLToken(string: string) {
 	return string.replace(/[^A-Za-z0-9]/g, '--');
 }
